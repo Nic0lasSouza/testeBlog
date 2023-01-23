3df4ab87c67a9bfc3de71590e62611d2d309927d
Nicolas Souza <96439452+Nic0lasSouza@users.noreply.github.com>
2023-01-23 16:31:42 -0300

    blog adicionado

--- main.js
+++ main.js
@@ -2,8 +2,19 @@ const navItems = document.querySelector('.nav__items');
 const openNavBtn = document.querySelector('#open__nav-btn');
 const closeNavBtn = document.querySelector('#close__nav-btn');
 
-
+//open nav btn
 const openNav = ()=>{
     navItems.style.display = 'flex';
+    openNavBtn.style.display ='none';
+    closeNavBtn.style.display = 'inline-block';
 }
+
+//close nav btn
+const  closeNav= ()=>{
+    navItems.style.display = 'none';
+    openNavBtn.style.display ='inline-block';
+    closeNavBtn.style.display = 'none';
+}
+
 openNavBtn.addEventListener('click', openNav);
+closeNavBtn.addEventListener('click', closeNav)
